071e8d15a7f75c45d5a5eed152dd11da45f77874
Meshack-Commey <commeymeshack@gmail.com>
2022-03-07 03:21:19 +0000

    All in one project

---
+++ HTML/WebDesignCodeTemplates/workstation/MeshDesktop.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>https://www.webdesktop.com/server-side desktop/</title>
+    <link rel="stylesheet" href="css/MeshDesktop.css"/>
+</head>
+<body>
+    <section class="MeshDesktop">
+        <header>web desktop<!--A server-side desktop--></header>
+        <div class="Desktop"></div>
+        <div class="desktop-neck"><!--neck of desktop-->
+            <center><br>
+                <span>switch</span><a href="ON.php"><button type="button">on</button></a><br>
+            </center>
+        </div>
+        <div class="desktop-base"> <!--base of desktop-->
+
+        </div>
+    </section>
+</body>
+</html>
---
+++ HTML/WebDesignCodeTemplates/workstation/ON.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>https://www.webdesktop.com/server-side desktop/</title>
+    <link rel="stylesheet" href="css/MeshDesktop.css"/>
+</head>
+<body>
+    <section class="MeshDesktop">
+        <header>web desktop</header>
+        <div class="Desktop"><iframe name="webframe" src="desktop.png"></iframe></div>
+        <div class="desktop-neck"><!--neck of desktop-->
+                <p style="margin-left: 5px;">
+                    <span>switch</span><a href="MeshDesktop.php"><button>off</button></a>
+                    <div class="settings">settings</div>
+                </p>
+        </div>
+        <div class="desktop-base"> <!--base of desktop-->
+
+        </div>
+    </section>
+</body>
+</html>
